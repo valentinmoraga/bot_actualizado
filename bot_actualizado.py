211a46180c9cc4b27b041ab5f73864ea9009b254
valentinmoraga <161959688+valentinmoraga@users.noreply.github.com>
2024-03-23 13:24:01 -0300

    Create bot_actualizado.py

---
+++ bot_actualizado.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+bot.run("MTIxODU4OTAzNDkxMDA2MDYyNw.GXI0e2.ybjufEwGN3mLBYPjlPitKGAD07jkk2Ws0WL85w")
